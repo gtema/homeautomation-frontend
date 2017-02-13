9c0ceae261e877ad6bc2e5a47e284b8b53d28a59
artemgoncharov <artem.goncharov@gmail.com>
2017-02-13 17:30:27 +0100

    deploy helpers

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-REACT_APP_API_HOST=fast-savannah-53552.herokuapp.com REACT_APP_API_PATH=/api/v0/stock/ REACT_APP_API_AUTH=/auth npm run build
-rsync -av --del build/* pi:/var/www/html
